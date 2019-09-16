e5f8c4ec1c892600ff2af1b0e1a851f3577b17a3
Nickolas Fuentes <nickolasfuentes@Nickolass-MBP.attlocal.net>
2019-09-15 21:53:17 -0500

    changed the datatypes for the table and created a index route to get all the locaitons.

--- server/app.js
+++ server/app.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const models = require('./models')
 
 app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello')
+    
+    models.Hike.findAll({
+    }).then(hikes => {
+        res.json(hikes)
+    })
 })
 
 app.listen(3001, () => {
-    console.log('Server is running...')
+    console.log("Server is running...")
 })
--- server/models/hike.js
+++ server/models/hike.js
@@ -1,8 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Hike = sequelize.define('Hike', {
-    latitude: DataTypes.INTEGER,
-    longitude: DataTypes.INTEGER
+    latitude: DataTypes.DECIMAL,
+    longitude: DataTypes.DECIMAL
   }, {});
   Hike.associate = function(models) {
     // associations can be defined here
